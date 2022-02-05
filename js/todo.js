e06b30d0535e917ac8f21038c62ce049980274a6
jinniecode <jinniecode@gmail.com>
2022-02-05 23:33:01 +0900

    tododel

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "toDos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   // console.log("Pepepee")
   const li = event.target.parentElement;
@@ -28,7 +36,25 @@ function handleToDoSubmit(event) {
   // console.log(toDoInput.value);
   toDoInput.value = "";
   // console.log(newTodo, toDoInput.value);
+  toDos.push(newTodo);
   paintToDO(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
+/*  arrow function으로 대신함
+function sayHello(item) {
+  console.log("this is the turn of", item);
+}
+*/
+const savedToDos = localStorage.getItem(TODOS_KEY);
+//console.log(savedToDos);
+
+if (savedToDos/*==! null*/) {
+  const parsedToDos = JSON.parse(savedToDos);
+  //console.log(parsedToDos);
+  //parsedToDos.forEach(sayHello);
+  //parsedToDos.forEach((item) => console.log("this is the turn of", item));
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDO);
+}
